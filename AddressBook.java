580cb503a70d5277cbc52c6cbc33d36629593ac2
lokeshny <lokeshny97@gmail.com>
2022-04-22 23:02:59 +0530

    Added:Intial commit

---
+++ AddressBook.java
@@ -0,0 +1,13 @@
+package com.bridgelab.Addres;
+
+import java.util.*;
+
+public class AddressBook {
+
+    Scanner sc = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        AddressBook ab = new AddressBook();
+        System.out.println("Welcome to Address Book Program");
+    }
+}
---
+++ Contacts.java
@@ -0,0 +1,12 @@
+package com.bridgelab.Addres;
+
+    public class Contacts {
+        private String firstname;
+        private String lastName;
+        private String address;
+        private String city;
+        private String state;
+        private String zip;
+        private String phoneNumber;
+        private String email;
+}
